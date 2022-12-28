f2f49a551c84d17c7fe4fb923ccc25a2df12fd13
Shruti Pistolwala <shruti.pistolwala@tum.de>
2022-12-28 12:21:02 +0100

    interactable

---
+++ VR_Interactions/Assets/PlayPause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using Valve.VR;
+using UnityEngine.Video;
+// using System;
+
+public class PlayPause : MonoBehaviour       
+{
+    private VideoPlayer video;
+
+    private void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    public void PlayVideo()
+    {
+        video.Play();
+        // Console.WriteLine("x is greater than y");
+
+    }
+    public void PauseVideo()
+    {
+        video.Pause();
+        // Console.WriteLine("x is greater than y");
+    }
+}
