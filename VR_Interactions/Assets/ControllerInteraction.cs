5854bb517f64f087c729481cf9fc156154054442
Shruti Pistolwala <shruti.pistolwala@tum.de>
2023-01-04 19:14:25 +0100

    controller

--- VR_Interactions/Assets/ControllerInteraction.cs
+++ VR_Interactions/Assets/ControllerInteraction.cs
@@ -7,10 +7,23 @@ using Valve.VR;
 public class ControllerInteraction : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+
+    private GameObject collidingObject; // 1
+    private GameObject objectInHand; // 2
+
+    private void SetCollidingObject(Collider col)
+{
+    // 1
+    if (collidingObject || !col.GetComponent<Rigidbody>())
     {
-        
+        return;
     }
+    // 2
+    collidingObject = col.gameObject;
+}
 
     // Update is called once per frame
     void Update()
