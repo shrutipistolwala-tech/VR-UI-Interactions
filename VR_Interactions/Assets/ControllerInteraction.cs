7a9b698f0d1cba45a7bfc54e70517e1a85ae84e1
Shruti Pistolwala <shruti.pistolwala@tum.de>
2023-01-09 13:56:39 +0100

    Camera Rig working

--- VR_Interactions/Assets/ControllerInteraction.cs
+++ VR_Interactions/Assets/ControllerInteraction.cs
@@ -1,33 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
+//using Valve.VR;
 
 
 public class ControllerInteraction : MonoBehaviour
 {
     // Start is called before the first frame update
-    public SteamVR_Input_Sources handType;
-    public SteamVR_Behaviour_Pose controllerPose;
-    public SteamVR_Action_Boolean grabAction;
+    /// <summary>
+    /// 
+    /// </summary>
+   // public SteamVR_Input_Sources handType;
+   // public SteamVR_Behaviour_Pose controllerPose;
+   // public SteamVR_Action_Boolean grabAction;
+  //  private GameObject collidingObject; // 1
+  //  private GameObject objectInHand; // 2
 
-    private GameObject collidingObject; // 1
-    private GameObject objectInHand; // 2
-
-    private void SetCollidingObject(Collider col)
-{
+   // private void SetCollidingObject(Collider col)
+//{
     // 1
-    if (collidingObject || !col.GetComponent<Rigidbody>())
-    {
-        return;
-    }
+   // if (collidingObject || !col.GetComponent<Rigidbody>())
+   // {
+        //return;
+   // }
     // 2
-    collidingObject = col.gameObject;
-}
+  //  collidingObject = col.gameObject;
+//}
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
+   // }
 }
